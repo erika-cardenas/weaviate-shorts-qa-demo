315db595df3cb782032d4c672f204d62c3e886f8
Erika Cardenas <110841617+erika-cardenas@users.noreply.github.com>
2023-01-27 15:01:56 -0500

    Version 1 of demo

---
+++ schema.py
@@ -0,0 +1,34 @@
+import weaviate
+import json 
+
+client = weaviate.Client("http://localhost:8080")
+
+
+class_obj = {
+    "class": "WeaviateShorts", 
+    "description": "Quick answers to various questions",
+    "properties": [
+        {
+            "name": "question",
+            "dataType": ["string"],
+            "description": "Questions from the document",
+        },
+        {
+            "name": "answer",
+            "dataType": ["string"],
+            "description": "Answers to the questions",
+        }
+    ]
+}
+
+# resetting the schema 
+client.schema.delete_all()
+
+# add the schema
+client.schema.create_class(class_obj)
+
+# get the schema
+schema = client.schema.get()
+
+# print the schema
+print(json.dumps(schema, indent=4))
---
+++ upload.py
@@ -0,0 +1,18 @@
+import weaviate
+import json
+
+client = weaviate.Client("http://localhost:8080")
+
+with open('data.json', 'r') as f:
+    data = json.load(f, strict = False)
+
+
+for shorts in data["WeaviateShorts"]:
+  properties = {
+    "question": shorts["question"],
+    "answer": shorts["answer"]
+  }
+  client.data_object.create(
+    data_object=properties,
+    class_name= "WeaviateShorts"
+  )
